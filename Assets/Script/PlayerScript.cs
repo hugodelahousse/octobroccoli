7e162a0993b39b349b1285b7f430ef7c719d64ae
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-02-11 15:03:56 +0100

    Player: Add movement script

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private bool isGrounded = true;
+    
+    private Rigidbody2D rb;
+    private string gravityButton = "gravity_P1"; 
+
+    [SerializeField]
+    LayerMask groundLayer;
+
+    [SerializeField]
+    Vector2 playerVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = rb.IsTouchingLayers(groundLayer);
+        if (isGrounded && Input.GetButtonDown(gravityButton))
+            rb.gravityScale *= -1;
+
+        Debug.Log(playerVelocity);
+        rb.velocity = new Vector2(playerVelocity.x, rb.velocity.y);
+        Debug.Log(rb.velocity);
+    }
+
+    void Update()
+    {
+
+    }
+}
