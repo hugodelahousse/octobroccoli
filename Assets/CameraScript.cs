5369b97fb8246c55dd548a01c9c73bc11bd35d3a
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-02-11 15:10:57 +0100

    Camera: Add simple camera script

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 translation;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += translation * Time.deltaTime;
+    }
+}
