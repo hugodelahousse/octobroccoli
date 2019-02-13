882ce5a5db0a1c84f65e484c6228241149080ec1
Adrien Hellec <adrien.hellec@epita.fr>
2019-02-13 18:13:11 +0100

    Camera: detect player outside of screen and destroy it

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,9 +7,28 @@ public class CameraScript : MonoBehaviour
     [SerializeField]
     Vector3 translation;
 
+    [SerializeField]
+    GameObject[] players;
+
+    private bool end = false;
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += translation * Time.deltaTime;
+        if (!end) {
+            checkDeath();
+            transform.position += translation * Time.deltaTime;
+        }
+    }
+
+    void checkDeath() {
+        foreach (GameObject p in players) {
+            if (end || Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x >= p.transform.position.x
+                    || Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y >= p.transform.position.y
+                    || Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).y <= p.transform.position.y
+            ) {
+                end = true;
+                Destroy(p);
+            }
+        }
     }
 }
