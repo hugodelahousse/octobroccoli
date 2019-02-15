e021000d77c0ab28c69966076248ab4cbbf8683b
Adrien Hellec <adrien.hellec@epita.fr>
2019-02-15 15:25:25 +0100

    UI: add score on game screen

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    private int distance = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+        if (players.Length > 0) {
+            distance++;
+            GetComponent<UnityEngine.UI.Text>().text = distance.ToString();
+        }
+    }
+}
