e0f0ed707d2ee5718e0d2549dd093c15205e7d46
Adrien Hellec <adrien.hellec@epita.fr>
2019-02-15 15:51:05 +0100

    UI: add game over ui

---
+++ Assets/Scripts/CheckEnd.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckEnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool enabled = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!enabled) {
+            GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+            if (players.Length == 0) {
+                GetComponent<Canvas>().enabled = true;
+                enabled = true;
+            }
+        }
+    }
+}
