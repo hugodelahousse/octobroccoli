f2ce08fa4c439877e3156f783e3840165edd6f4c
Adrien Hellec <adrien.hellec@epita.fr>
2019-02-15 16:12:46 +0100

    Menu: add sounds and camera movement

---
+++ Assets/Scripts/CameraScriptMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScriptMenu : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 translation;
+
+    private bool reverse = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > 1000)
+            reverse = true;
+        if (transform.position.x < 200)
+            reverse = false;
+        if (reverse)
+            transform.position -= translation;
+        else
+            transform.position += translation;
+    }
+}
